03e372ac97a86a4237e791a32c3c58a5eac9398a
David Skotte <david.skotte@gmail.com>
2022-04-04 16:21:33 +0200

    fått ihop en Register + Profile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { authState } from "../../recoil/auth/atom";
+import { useRecoilValue } from "recoil";
 
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Badge from "@mui/material/Badge";
-import Box from "@mui/material/Box";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 import logo from "../../assets/logo.png";
 
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+
+
+
+
+
+
+
+  
 const Navbar = ({ totalItems }) => {
+  const auth = useRecoilValue(authState);
+  console.log(auth);
   return (
     <Box sx={{ display: "flex", flexGrow: 1 }}>
       <AppBar position="static">
@@ -20,6 +53,9 @@ const Navbar = ({ totalItems }) => {
           <Link to="/login">Login</Link>
           <Link to="/products">Products</Link>
           <Link to="/cart">Cart</Link>
+          {auth.token ? <Link to="/profile">Profile</Link> : null}
+          {auth.user.role === "admin" ? <Link to="/admin">Admin</Link> : null}
+          <Link to="/register">Register</Link>
         </nav>
 
         <Toolbar sx={{ justifyContent: "space-between" }}>
